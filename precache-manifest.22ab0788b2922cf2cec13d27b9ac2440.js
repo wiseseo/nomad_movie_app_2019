e4d2c6435d4eb156a1c111b19868caf323e96e47
JihyeSeo1120 <mansesjh22@gmail.com>
2019-08-25 11:45:29 +0900

    Updates

---
+++ precache-manifest.22ab0788b2922cf2cec13d27b9ac2440.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6cd900085d7f15b41fb5478b0e13b1",
+    "url": "/nomad_movie_app_2019/index.html"
+  },
+  {
+    "revision": "fd4efbbfcbbc14d63928",
+    "url": "/nomad_movie_app_2019/static/css/main.de4c14a3.chunk.css"
+  },
+  {
+    "revision": "f0a3eedea261ff286d1b",
+    "url": "/nomad_movie_app_2019/static/js/2.aa826c8a.chunk.js"
+  },
+  {
+    "revision": "fd4efbbfcbbc14d63928",
+    "url": "/nomad_movie_app_2019/static/js/main.e69f9df6.chunk.js"
+  },
+  {
+    "revision": "d64862b452a33a70b61e",
+    "url": "/nomad_movie_app_2019/static/js/runtime~main.44e567f6.js"
+  }
+]);
